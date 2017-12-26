3168c401b296f5082ec61e44696b225d695a2284
fuxins <33921425+fuxins@users.noreply.github.com>
2017-12-26 15:07:52 +0800

    listgene.hpa.r

---
+++ listgene/listgene.hap.r
@@ -0,0 +1,29 @@
+library(readr)
+
+#in cell atlas
+membrane <- rbind(plasma,celljunc)%>%
+  unique()
+
+
+#in protein classes
+protein_class_predicted <- read_tsv("./HPA/protein_class_Predicted.tsv.gz")
+
+protein_class_predicted %>%
+  filter(is.na(`Subcellular location`))-> protein.1
+
+protein_class_predicted %>%
+  filter(`Subcellular location`=="Plasma membrane") ->protein.membrn
+
+
+protein_class_predicted %>%
+  filter(`Subcellular location`=="Vesicles<br>Plasma membrane<br>Mitochondria") %>%
+  rbind(protein.plmembrn)-> protein.plmembrn
+
+protein.1 %>%
+  rbind(protein.celljunction) %>%
+  rbind(protein.membrn) %>%
+  rbind(protein.plmembrn)->protein.m
+
+write_tsv(protein.m,"./genelist_hap.tsv")
+write_tsv(membrane,"./genelist_hap.ca.tsv")
+ 
